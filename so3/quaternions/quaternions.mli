f47a0829407fed4303c5ddb338f96ba23b7f4337
hennequin <gje.hennequin@gmail.com>
2019-11-18 15:36:10 +0000

    wrote documentation for quaternions

--- so3/quaternions/quaternions.mli
+++ so3/quaternions/quaternions.mli
@@ -1,8 +1,20 @@
 open Owl
 
+(**
+Here is an example of how to use this library:
+
+{[
+let qs = Array.init 1000 (fun _ -> Q.sample ())
+let qs3D = Array.map Q.to3D qs |> Mat.concatenate ~axis:0
+let _ = Mat.save_txt qs3D (in_dir "qs")
+]}
+
+*)
+
 type t
 
-(** [create theta u] makes a unit quaternion; [u] is first normalised so only its direction matters. *)
+(** [create theta u] makes a unit quaternion; [u] is first normalised so only its direction matters.
+    @return a fresh quaternion *)
 val create : float -> Mat.mat -> t
 
 val dot_prod : t -> t -> float
@@ -16,5 +28,6 @@ val geodesic_distance : t -> t -> float
 (** place a quaternion inside the 3D ball used to visualise them *)
 val to3D : t -> Mat.mat
 
-(** sample quaternions from a geodesic-uniform distribution on SO(3) *)
+(** sample quaternions from a geodesic-uniform distribution on SO(3);
+    this function calls {!create} under the hood. *)
 val sample : unit -> t
